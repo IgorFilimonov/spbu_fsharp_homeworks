1c46f38939629b49510aa72f122fc4a9332eb946
IgorFilimonov <st087840@student.spbu.ru>
2023-09-03 23:20:55 +0300

    Added solution

---
+++ hw4/task2/task2/Program.fs
@@ -0,0 +1,9 @@
+﻿open FsCheck
+
+let startingFunc x l = List.map (fun y -> y * x) l
+let func1 x l = List.map (fun y -> y * x)
+let func2 x = List.map ((*) x)
+let func3 = (*) >> List.map
+
+let checkEquality x l = startingFunc x l = func3 x l
+Check.QuickThrowOnFailure checkEquality
