1049d1947e4d594aec0d3dfa9e3f22cff108120a
IgorFilimonov <st087840@student.spbu.ru>
2023-09-03 12:56:12 +0300

    Added solution

---
+++ hw2/task4/task4/Program.fs
@@ -0,0 +1,24 @@
+﻿module PrimeSequence
+
+let isPrime number =
+    let rec isPrimeRecursive divider =
+        if (divider * divider > number) then
+            true
+        elif (number % divider = 0) then
+            false
+        else
+            isPrimeRecursive (divider + 1)
+    isPrimeRecursive 2
+
+let findPrimeByIndex index =
+    let rec findPrimeByIndexRecursive leftToFind current =
+        if (isPrime current = true) then
+            if (leftToFind = 1) then
+                current
+            else
+                findPrimeByIndexRecursive (leftToFind - 1) (current + 1)
+        else findPrimeByIndexRecursive leftToFind (current + 1)
+    findPrimeByIndexRecursive (index + 1) 2
+
+let getSeqOfPrimes() = Seq.initInfinite (fun index -> findPrimeByIndex index)
+printfn "%A" (getSeqOfPrimes() |> Seq.take 10 |> Seq.toList)
