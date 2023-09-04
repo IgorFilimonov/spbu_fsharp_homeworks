98d4bd0c67285f3f3756be0b6dd4b7e0d921e37f
IgorFilimonov <st087840@student.spbu.ru>
2023-09-04 13:22:22 +0300

    .

--- hw1/hw1.1/hw1.1/Program.fs
+++ hw1/hw1.1/hw1.1/Program.fs
@@ -1,14 +1,15 @@
 ﻿let factorial number =
-    let rec tailRecursiveFactorial number acc =
-        if number <= 1 then
-            acc
-        else
-            tailRecursiveFactorial (number - 1) (acc * number)
-    tailRecursiveFactorial number 1
+    if (number < 0) then None
+    else 
+        let rec tailRecursiveFactorial number acc =
+            if number <= 1 then
+                acc
+            else
+                tailRecursiveFactorial (number - 1) (acc * number)
+        Some (tailRecursiveFactorial number 1)
 
 printfn "Enter a number you want to get a factorial of:"
 let number = System.Console.ReadLine() |> int
-if (number < 0) then
-    printfn "There's no factorial of a negative number"
-else
-    printfn "Factorial of %d = %d" number (factorial number)
+match factorial number with
+| Some x -> printfn "Factorial of %d = %d" number x
+| None -> printfn "There's no factorial of a negative number"
