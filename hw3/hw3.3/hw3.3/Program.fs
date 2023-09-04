bf9cbf131a1e63035216e73b4f78dda9ae3f908c
IgorFilimonov <st087840@student.spbu.ru>
2023-09-04 16:28:15 +0300

    Fixed beta-reduction

--- hw3/hw3.3/hw3.3/Program.fs
+++ hw3/hw3.3/hw3.3/Program.fs
@@ -16,13 +16,13 @@ let substitute term old replacement =
     let rec substituteResursive term bound = 
         match term with
         | Variable(var) -> 
-            if (var = old) then replacement
-            elif (Set.contains var bound) then Variable("'" + var)
+            if (var = old && not (Set.contains var bound)) then replacement
+            elif (Set.contains var freeVars && Set.contains var bound) then Variable("'" + var)
             else Variable(var)
         | Application(a, b) -> Application(substituteResursive a bound, substituteResursive b bound)
         | LambdaAbstraction(a, b) -> 
             if (Set.contains a freeVars) then LambdaAbstraction("'" + a, substituteResursive b (Set.add a bound))
-            else LambdaAbstraction(a, substituteResursive b bound)
+            else LambdaAbstraction(a, substituteResursive b (Set.add a bound))
     substituteResursive term Set.empty
 
 let rec reduce term = 
