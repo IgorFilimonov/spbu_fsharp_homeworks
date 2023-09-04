5fd5d1c827cb7eec6926c3837a02ca88a75ffcba
IgorFilimonov <st087840@student.spbu.ru>
2023-09-04 14:19:00 +0300

    added comms

--- hw4/task2/task2/Program.fs
+++ hw4/task2/task2/Program.fs
@@ -1,7 +1,7 @@
 ﻿open FsCheck
 
 let startingFunc x l = List.map (fun y -> y * x) l
-let func1 x l = List.map (fun y -> y * x)
+let func1 x = List.map (fun y -> y * x) // -> (fun y -> (*) x y)
 let func2 x = List.map ((*) x)
 let func3 = (*) >> List.map
 
