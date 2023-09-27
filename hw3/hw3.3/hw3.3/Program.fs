77b2f669ca30d7217b62b9b8ae0e8af383445828
IgorFilimonov <st087840@student.spbu.ru>
2023-09-27 21:55:47 +0300

    .

--- hw3/hw3.3/hw3.3/Program.fs
+++ hw3/hw3.3/hw3.3/Program.fs
@@ -11,19 +11,27 @@ let rec getFreeVars term =
     | Application(a, b) -> Set.union (getFreeVars a) (getFreeVars b)
     | LambdaAbstraction(a, b) -> (getFreeVars b) |> Set.remove a
 
+let getNewName var freeVars =
+    let rec loop name =
+        if (Set.contains name freeVars) then
+            loop ("'" + name)
+        else
+            name
+    loop var
+
 let substitute term old replacement = 
     let freeVars = getFreeVars replacement
-    let rec substituteResursive term bound = 
+    let rec substituteRecursive term bound = 
         match term with
         | Variable(var) -> 
             if (var = old && not (Set.contains var bound)) then replacement
-            elif (Set.contains var freeVars && Set.contains var bound) then Variable("'" + var)
+            elif (Set.contains var freeVars && Set.contains var bound) then Variable(getNewName var freeVars)
             else Variable(var)
-        | Application(a, b) -> Application(substituteResursive a bound, substituteResursive b bound)
+        | Application(a, b) -> Application(substituteRecursive a bound, substituteRecursive b bound)
         | LambdaAbstraction(a, b) -> 
-            if (Set.contains a freeVars) then LambdaAbstraction("'" + a, substituteResursive b (Set.add a bound))
-            else LambdaAbstraction(a, substituteResursive b (Set.add a bound))
-    substituteResursive term Set.empty
+            if (Set.contains a freeVars) then LambdaAbstraction(getNewName a freeVars, substituteRecursive b (Set.add a bound))
+            else LambdaAbstraction(a, substituteRecursive b (Set.add a bound))
+    substituteRecursive term Set.empty
 
 let rec reduce term = 
     match term with
