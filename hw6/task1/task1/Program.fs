c96d07eaff911876aa601aaf8a8640aba1431029
IgorFilimonov <st087840@student.spbu.ru>
2023-10-06 00:04:58 +0300

    Added solution

---
+++ hw6/task1/task1/Program.fs
@@ -0,0 +1,8 @@
+﻿module ComputeWithPrecision
+
+open System
+
+type ComputationBuilder(precision: int) =
+    let round (value: float) = Math.Round(value, precision)
+    member this.Bind(value, func) = func (round value)
+    member this.Return(value) = round value
