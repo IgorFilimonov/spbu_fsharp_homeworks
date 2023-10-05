c122bcf3d8d3a3e22b42b07cc5b8c14484ec5c9f
IgorFilimonov <st087840@student.spbu.ru>
2023-10-06 00:25:41 +0300

    Added solution

---
+++ hw6/task2/task2/Program.fs
@@ -0,0 +1,14 @@
+﻿module StringComputation
+
+open System
+
+type StringComputationBuilder() =
+    let parse (s: string) =
+        match Int32.TryParse s with
+        | true, value -> Some value
+        | _ -> None
+    member this.Bind(value, func) =
+        match parse value with
+        | None -> None
+        | Some value -> func value
+    member this.Return(value) = Some value
