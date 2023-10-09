90ea90a9d0b8b05a2e66331c0a9ec8ee596a147e
IgorFilimonov <st087840@student.spbu.ru>
2023-10-09 18:29:12 +0300

    Fixed issues

--- hw6/task2/task2/Program.fs
+++ hw6/task2/task2/Program.fs
@@ -7,8 +7,8 @@ type StringComputationBuilder() =
         match Int32.TryParse s with
         | true, value -> Some value
         | _ -> None
-    member this.Bind(value, func) =
+    member _.Bind(value, func) =
         match parse value with
         | None -> None
         | Some value -> func value
-    member this.Return(value) = Some value
+    member _.Return(value) = Some value
